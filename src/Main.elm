fffcb9e6c256b256a87b385e20c064b0faaf6934
mohamadyoussefio <ymohamadry@gmail.com>
2026-01-29 12:34:12 +0100

    Format Main

--- src/Main.elm
+++ src/Main.elm
@@ -7,26 +7,29 @@ import Html.Events exposing (..)
 import Turtle.Parser as Parser
 import Turtle.View as View
 
--- MODEL
+
 type alias Model =
     { currentInput : String
-    , committedInput : String 
+    , committedInput : String
     }
+
 
 type Msg
     = InputChanged String
     | DrawClicked
-    | ClearClicked  -- 1. NEW MESSAGE
+    | ClearClicked
+
 
 main =
     Browser.sandbox
-        { init = 
+        { init =
             { currentInput = "[Repeat 360 [Forward 1, Left 1]]"
-            , committedInput = "" 
+            , committedInput = ""
             }
         , update = update
         , view = view
         }
+
 
 update : Msg -> Model -> Model
 update msg model =
@@ -37,53 +40,54 @@ update msg model =
         DrawClicked ->
             { model | committedInput = model.currentInput }
 
-        ClearClicked -> -- 2. HANDLE CLEAR LOGIC
-            { model 
-            | currentInput = "" 
-            , committedInput = "" 
+        ClearClicked ->
+            { model
+                | currentInput = ""
+                , committedInput = ""
             }
+
 
 view : Model -> Html Msg
 view model =
     div [ class "container" ]
         [ h1 [] [ text "TcTurtle" ]
-
         , div [ class "workspace" ]
-            [ -- LEFT: Input Area
-              div [ class "input-section" ]
+            [ div [ class "input-section" ]
                 [ p [] [ text "Enter Command:" ]
-                , textarea 
+                , textarea
                     [ value model.currentInput
                     , onInput InputChanged
                     , spellcheck False
-                    ] []
-                
-                -- 3. BUTTONS ROW
-                , div [ class "button-row" ] 
+                    ]
+                    []
+                , div [ class "button-row" ]
                     [ button [ onClick DrawClicked, class "btn-primary" ] [ text "DRAW FIGURE" ]
                     , button [ onClick ClearClicked, class "btn-secondary" ] [ text "CLEAR" ]
                     ]
-                
                 , div [ style "font-size" "12px", style "margin-top" "10px" ]
                     [ text "Status: "
                     , case Parser.parse model.currentInput of
-                        Ok _ -> span [ style "color" "green" ] [ text "READY" ]
-                        Err _ -> span [ style "color" "red" ] [ text "SYNTAX ERROR" ]
+                        Ok _ ->
+                            span [ style "color" "green" ] [ text "READY" ]
+
+                        Err _ ->
+                            span [ style "color" "red" ] [ text "SYNTAX ERROR" ]
                     ]
                 ]
-
-            -- RIGHT: Canvas Area
-            , View.viewCanvas 
+            , View.viewCanvas
                 (case Parser.parse model.committedInput of
-                    Ok instructions -> instructions
-                    Err _ -> [])
+                    Ok instructions ->
+                        instructions
+
+                    Err _ ->
+                        []
+                )
             ]
-            , div [ class "footer" ]
-                [ text "Created by " 
-                , strong [] [ text "Mohamad Y. & Majid M." ] -- Put your name here
-                , text " | "
-                , a [ href "https://github.com/mohamadyoussefio/tc-turtle", target "_blank" ] 
+        , div [ class "footer" ]
+            [ text "Created by "
+            , strong [] [ text "Mohamad Y. & Majid M." ]
+            , text " | "
+            , a [ href "https://github.com/mohamadyoussefio/tc-turtle", target "_blank" ]
                 [ text "View Source on GitHub" ]
             ]
         ]
-
